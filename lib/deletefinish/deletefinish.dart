b99e2cafae30ccd1236f7c7ebac07e0573d5eeaa
Aedli <willy0429@naver.com>
2024-12-01 16:15:48 +0900

    회원정보에 이름하고 아이디 나오는 기능 추가 현재 비번 입력후 새 비번 2번 입력하면 비번 변경하게 만듦 회원 탈퇴시 비밀번호 입력하면 탈퇴되고 로그인창으로 돌아가게 만드는 기능 추가 코드 오류 수정

--- lib/deletefinish/deletefinish.dart
+++ lib/deletefinish/deletefinish.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:mypage/screens/login_screen.dart';
 
 class DeleteFinish extends StatelessWidget {
